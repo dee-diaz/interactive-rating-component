71cfdc20c40d715a0b035131f29e5194c733d415
dee-diaz <diazthediablo@yahoo.com>
2023-06-18 16:43:52 -0300

    Modified the script

--- script.js
+++ script.js
@@ -13,19 +13,34 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
-
     submitBtn.addEventListener('click', () => {
         cardCircles.forEach(circle => {
             if(circle.classList.contains('active')) {
-                card.innerHTML = `
-                    <div class="card_submitted">
-                        <div class="card__img">
-                            <img src="src/img/illustration-thank-you.svg" alt="Illustration thank you">
-                        </div>
-                        <div class="card__your-rating">You selected ${circle.textContent} out of ${cardCircles.length}</div>
-                        <div class="card__title">Thank you!</div>
-                        <div class="card__text card__text2">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</div>
-                    </div>`
+                card.innerHTML = '';
+                card.classList.add('card_submitted');
+                
+                const cardImg = document.createElement('div');
+                cardImg.classList.add('card__img');
+                const img = document.createElement('img');
+                img.src = 'src/img/illustration-thank-you.svg';
+                img.alt = 'Thank you';
+                cardImg.appendChild(img);
+                card.appendChild(cardImg);
+
+                const yourRating = document.createElement('div');
+                yourRating.classList.add('card__your-rating');
+                yourRating.textContent = `You selected ${circle.textContent} out of ${cardCircles.length}`;
+                card.appendChild(yourRating);
+
+                const title = document.createElement('div');
+                title.classList.add('card__title');
+                title.textContent = 'Thank you!';
+                card.appendChild(title);
+
+                const text = document.createElement('div');
+                text.classList.add('card__text', 'card__text2');
+                text.textContent = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
+                card.appendChild(text);
             }
         })
     })
